b72af2c6c8751232b221eb79e923348bb0e3d962
Kevin Sanji <kevinsanji@Kevins-MacBook-Pro.local>
2018-07-02 18:00:11 -0700

    Added fake server data for playlist and hours counter components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,69 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Aggregate extends Component {
+let fakeServerData = {
+  user: {
+    name: 'Kevin',
+    playlists: [
+      {
+        name: 'My favorites',
+        songs: [
+          {name: 'Song1', duration: 1234},
+          {name: 'Song2', duration: 1234},
+          {name: 'Song3', duration: 1234}
+        ]
+      },
+      {
+        name: 'Discover weekly',
+        songs: [
+          {name: 'Song1', duration: 1234},
+          {name: 'Song2', duration: 1234},
+          {name: 'Song3', duration: 1234}
+        ]
+      },
+      {
+        name: 'Discover weekly',
+        songs: [
+          {name: 'Song1', duration: 1234},
+          {name: 'Song2', duration: 1234},
+          {name: 'Song3', duration: 1234}
+        ]
+      },
+      {
+        name: 'Discover weekly',
+        songs: [
+          {name: 'Song1', duration: 1234},
+          {name: 'Song2', duration: 1234},
+          {name: 'Song3', duration: 1234}
+        ]
+      }
+    ]
+  }
+};
+
+class PlaylistCounter extends Component {
   render() {
     return (
       <div>
-        <h2>Number Text</h2>
+        <h2>{this.props.playlists.length}</h2>
       </div>
     )
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    }, []);
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration;
+    }, 0)
+    return (
+      <div>
+        <h2>{Math.round(totalDuration/60)} hours</h2>
+      </div>
+    );
   }
 }
 
@@ -37,17 +93,33 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({serverData: fakeServerData});
+    }, 1000);
+  }
   render() {
     let name = 'Kevin'
     return (
       <div className="App">
-        <h1>Spotify Component Title</h1>
-        <Aggregate/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
+        {this.state.serverData.user ?
+        <div>
+          <h1>
+            {this.state.serverData.user.name}
+          </h1>
+          <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
+          <HoursCounter playlists={this.state.serverData.user.playlists}/>
+          <Filter/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+        </div> : <h1>'Loading'</h1>
+        }
       </div>
     );
   }
